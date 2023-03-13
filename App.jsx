a9f0d9c99b0ef3e59a6ebb95077e3f0b2eb244bd
dhaleprashika <124231072+dhaleprashika@users.noreply.github.com>
2023-03-13 21:52:52 +0530

    Add files via upload
    added

--- App.jsx
+++ App.jsx
@@ -1,21 +1,44 @@
+// import React from "react";
+// import { Component } from "react";
+
+// class App extends Component{
+
+//     constructor(){
+//         super()
+//             this.state={userName : "sharvari"}
+
+//     }
+
+//     render(){
+//         return(
+//         <>
+//         <h1 style={{color :"pink"}}>{this.state.userName}</h1>
+//         </>
+//         )
+
+//     }
+// }
+// export default App
+
 import React from "react";
 import { Component } from "react";
 
 class App extends Component{
-
-    constructor(){
-        super()
-            this.state={userName : "sharvari"}
-
+    state={
+        username : "nirmala",
+        // password : "nircivil2022"
+        skills : ["Java","SQL","JS"]
     }
 
     render(){
+        // console.log(this);
         return(
-        <>
-        <h1 style={{color :"pink"}}>{this.state.userName}</h1>
-        </>
+            <>
+            <h1>{this.state.username}</h1>
+            {/* <h2>{this.state.password}</h2> */}
+            <ul>{this.state.skills.map((x)=>{return <li>{x}</li>})}</ul>
+            </>
         )
-
     }
 }
 export default App
